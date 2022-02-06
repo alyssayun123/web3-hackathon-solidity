2030623d3d810e0e9ea46fd70b72f5117629412f
Channy <alyssay1226@gmail.com>
2022-02-05 20:04:30 -0800

    added sign payment function

--- scripts/stake.js
+++ scripts/stake.js
@@ -4,17 +4,42 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 const contract = require("../artifacts/contracts/Stake.sol/Stake.json");
 const contractAddress = "0xaDA41bB2DD8c0b79ceC8F9dF1e0eeAa2EF165621";
+const provider = new hre.ethers.providers.AlchemyProvider("maticmum");
 const Contract = new hre.ethers.Contract(
   contractAddress,
   contract.abi,
-  new hre.ethers.providers.AlchemyProvider("maticmum")
+  provider
 );
-async function main() {
-  const balance = await Contract.getBalance();
-  console.log(balance);
+
+async function signPayment(
+  recipient,
+  amount,
+  nonce,
+  _contractAddress,
+  callback
+) {
+  var hash = hre.ethers.utils
+    .solidityKeccak256(
+      ["address", "uint256", "uint256", "address"],
+      [recipient, amount, nonce, _contractAddress]
+    )
+    .toString("hex");
+  const [signer] = await ethers.getSigners();
+
+  signer.signMessage(hash, callback);
+}
+
+async function main(recipient) {
+  const amount = ethers.utils.parseUnits("0.001", 18);
+  await signPayment(recipient, amount, 1, contractAddress, () => {
+    console.log("done");
+  });
+  const owner = await Contract.getNonceOwner(1);
+  console.log(owner);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
